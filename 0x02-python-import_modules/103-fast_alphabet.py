35020df3ca1165b4bd6dece8e14ae21ba27d0ced
Emmoloks <emmokaloki77@gmail.com>
2024-04-03 02:14:24 -0700

    some guesswork

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 import string
-_ = __import__('os').write(1, bytes(getattr(string, 'ascii_uppercase'), 'utf-8') + b'\n')
+_=print(*map(chr, range(ord('A'), ord('Z') + 1)), sep='')
