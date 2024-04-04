41ea5cf1a1daf48bf1abf5308600da0668d095f6
Emmoloks <emmokaloki77@gmail.com>
2024-04-04 02:52:29 -0700

    fixing checkers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        my_list.reverse()
-    for num in my_list:
-        print("{:d}".format(num))
+    if type(my_list) is list:
+        reverse = my_list[0:]
+        reverse.reverse()
+        for i in range(len(reverse)):
+            print("{:d}".format(reverse[i]))
