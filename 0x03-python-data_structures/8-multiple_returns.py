e25655b0a342ebb50906586b71292bad020dc140
Emmoloks <emmokaloki77@gmail.com>
2024-04-10 04:34:45 -0700

    correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    str_len = len(sentence)
+    char_first = sentence[0]
     if sentence:
-        return(len(sentence), sentence[0])
+        return(str_len, char_first)
     else:
         return(0, None)
