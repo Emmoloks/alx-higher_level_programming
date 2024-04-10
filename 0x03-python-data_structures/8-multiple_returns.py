c37e2be66d408ebdd15274e6bb6cab7dd196c9b0
Emmoloks <emmokaloki77@gmail.com>
2024-04-10 04:53:41 -0700

    minor changes

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     str_len = len(sentence)
-    char_first = sentence[0]
-    return(str_len, char_first)
+
+    if sentence:
+        char_first = sentence[0]
+        return(str_len, char_first)
+    else:
+        char_first = None
