8ed612ecdb918a5962d07016b40fdf6043359a1f
Emmoloks <emmokaloki77@gmail.com>
2024-04-10 04:57:42 -0700

    fixing errors

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    str_len = len(sentence)
 
-    if sentence:
-        char_first = sentence[0]
-        return(str_len, char_first)
+def multiple_returns(sentence):
+    s_len = len(sentence)
+    if s_len == 0:
+        first_char = None
     else:
-        char_first = None
+        first_char = sentence[0]
+    return (s_len, first_char)
