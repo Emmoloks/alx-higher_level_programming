a8bd0dfb56da8e120d7d9b103118bc95573307bd
Emmoloks <emmanuelkaloki77@gmail.com>
2024-05-23 01:04:57 -0700

    fixing an error

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
---List score greater or equalto 10 from the table second_table
+-- List score greater or equalto 10 from the table second_table
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
