0e90e5d6c451fb6d4c2be98e8c6adbb0d4a11308
Emmoloks <emmanuelkaloki77@gmail.com>
2024-05-26 12:40:07 -0700

    fixing errors

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- a script that dispalys the top 3 cities temp
-SELECT city, temperature FROM your_table_name WHERE month IN(7, 8) ORDER BY temperature DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
